21464280df815370180c0cdb4922ccc71e7ad5f5
tonyxmacaronyx <antoniu1323@gmail.com>
2022-02-23 01:26:39 +0200

    End of section 8

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { IPagination } from './models/pagination';
+import { IProduct } from './models/product';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Shopping Store';
+  products: IProduct[];
+
+  // inject the http client in the parent class("AppComponent") so it can be used by all the child classes
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    //this = acces something available in this class.
+    this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe(
+      (response: IPagination) => {
+        this.products = response.data;
+      }, error => {
+        console.log(error);
+      });
+  }
 }
- 
