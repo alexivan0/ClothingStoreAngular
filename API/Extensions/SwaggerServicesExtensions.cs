417bab5b9c78bb4d36311b386fe13d43a986a280
tonyxmacaronyx <antoniu1323@gmail.com>
2022-03-08 23:39:39 +0200

    End of section 16

--- API/Extensions/SwaggerServicesExtensions.cs
+++ API/Extensions/SwaggerServicesExtensions.cs
@@ -12,7 +12,26 @@ namespace API.Extensions
         {
              services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "ShoppingStore API", Version = "v1" });
+
+                var securitySchema = new OpenApiSecurityScheme
+                {
+                    Description = "JWT Auth BEarer Scheme",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "bearer",
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    }
+                };
+
+                c.AddSecurityDefinition("Bearer", securitySchema);
+                var securityRequirement = new OpenApiSecurityRequirement {{securitySchema, new[]
+                {"Bearer"}}};
+                c.AddSecurityRequirement(securityRequirement);
             });
 
             return services;
@@ -21,7 +40,7 @@ namespace API.Extensions
         public static IApplicationBuilder UseSwaggerDocumentation(this IApplicationBuilder app)
         {
             app.UseSwagger();
-            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPIv5 v1"));
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ShoppingStore API v1"));
 
             return app;
         }
